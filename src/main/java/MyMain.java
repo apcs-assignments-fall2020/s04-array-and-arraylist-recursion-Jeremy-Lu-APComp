7a2c3aca3f276d30e9351b82dbb7854f27bf38a4
Jeremy-Lu-APComp <21jlu@athenian.org>
2021-02-06 18:04:08 +0000

    First commit

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,34 +4,41 @@ public class MyMain {
 
     // Wrapper method
     public static boolean isSorted(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return false;
+        return isSortedRec(list, 0);
     }
 
     // This recursive method checks if the array is sorted in 
     // non-decreasing order
     public static boolean isSortedRec(ArrayList<Integer> list, int i) {
-        // YOUR CODE HERE
-        return false;
+        if(i >= list.size()-1){
+            return true;
+        }
+        else if(list.get(i) > list.get(i+1)){
+            return false;
+        }
+        else{
+            return isSortedRec(list, i+1);
+        }
     }
-
-
 
      // Wrapper method
     public static boolean hasCountCopies(int[] arr, int x, int count) {
-        // YOUR CODE HERE
-        return false;
+        return hasCountCopiesRec(arr, x, count, 0);
     }
 
     // This recursive method checks if the array contains exactly
     // count copies of the integer x
     public static boolean hasCountCopiesRec(int[] arr, int x, int count, int i) {
-        // YOUR CODE HERE
-        return false;
-    }
-
-
-
+    if (arr.length-1 <= i) {
+		return count==0;
+	}
+	else if (arr[i] == x){
+        return hasCountCopiesRec(arr, x, count-1, i+1);
+	}
+	else {
+		return hasCountCopiesRec(arr, x, count, i+1);
+	}
+}
 
     // Wrapper method
     public static boolean binarySearch(int[] arr, int num) {
@@ -46,6 +53,14 @@ public class MyMain {
     }
 
     public static void main(String[] args) {
-        // You can test your code here
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(2);
+        arr.add(5);
+        int[] test_array = {1, 2, 2, 3, 4};
+        System.out.println(isSorted(arr));
+        System.out.println(hasCountCopies(test_array, 2, 2));
+        System.out.println(hasCountCopies(test_array, 2, 3));
     }
 }
